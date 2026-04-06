5fafffd8b1af8ca605ce3cd999848ed17d5a8d9e
王正宏 <apple1314520iphone@icloud.com>
2026-04-06 23:29:15 +0800

    feat: modify icons&fonts

--- src/fonts/AppleSDGothicNeo_ExtraBold_40px.c
+++ src/fonts/AppleSDGothicNeo_ExtraBold_40px.c
@@ -3220,7 +3220,6 @@ lv_font_t AppleSDGothicNeo_ExtraBold_40px = {
     .underline_position = -7,
     .underline_thickness = 2,
 #endif
-    .static_bitmap = 0,
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
     .fallback = NULL,
